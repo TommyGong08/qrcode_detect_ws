0a5a257816e9182edfd368e77864bb2030dbdede
TommyGONG08 <103423198@qq.com>
2020-11-04 02:07:25 -0800

    add src

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ghl/Desktop/qrcode_detect_ws/src
+# Build directory: /home/ghl/Desktop/qrcode_detect_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(qrcode_ros)
---
+++ build/qrcode_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ghl/Desktop/qrcode_detect_ws/src/qrcode_ros
+# Build directory: /home/ghl/Desktop/qrcode_detect_ws/build/qrcode_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
